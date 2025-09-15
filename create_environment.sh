87209f8aa46f598b233ad7e07e7236f45326f99b
Emil Östberg <emilo@wolf.matfys.lth.se>
2025-09-15 14:25:14 +0200

    created install script

---
+++ create_environment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#This script creates a environment where all the libraries required are installed. 
+#if you  cannot run the script make sure that you have done:
+#chmod +x script.sh
+#then it can be run with: ./script.sh
+curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+eval "$(./bin/micromamba shell hook -s bash)"
+
+micromamba create -y -p ./labenv
+
+micromamba activate ./labenv
+
+micromamba install -y numpy scipy matplotlib matplotlib-inline panel jupyter notebook ipython
+
+git clone https://github.com/emilost/Bandstructure
+cd Bandstructure
+jupyter notebook
+#Now open the files: 1D/Bandstructure-1D.ipynb 2D/Bandstructure-2DNoPopOut.ipynb
+

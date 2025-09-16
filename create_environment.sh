f1ec99dbdc890027bc3b8064f551f368537f320a
emilost <emil@firle.se>
2025-09-16 13:06:18 +0200

    Update create_environment.sh
    updated script

--- create_environment.sh
+++ create_environment.sh
@@ -2,8 +2,8 @@
 
 #This script creates a environment where all the libraries required are installed. 
 #if you  cannot run the script make sure that you have done:
-#chmod +x script.sh
-#then it can be run with: ./script.sh
+#chmod +x create_environment.sh
+#then it can be run with: ./create_environment.sh
 curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
 eval "$(./bin/micromamba shell hook -s bash)"
 
